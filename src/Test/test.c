3d5b1b3f4c413786d9a67225d17d67487b1f39be
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2013-07-03 18:00:55 -0500

    Implemented and debugged the tridiagonal solver in parallel

--- src/Test/test.c
+++ src/Test/test.c
@@ -1,33 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#ifndef serial
+#include <mpi.h>
+#endif
 #include <tridiagLU.h>
 
-static int    main_serial();
-static int    main_parallel();
 static void   CopyArray       (double*,double*,int);
+#ifdef serial
+static int    main_serial     (int);
 static double CalculateError  (double*,double*,double*,double*,double*,int);
+#else
+static int    main_mpi        (int,int,int);
+static int    partition1D     (int,int,int,int*);
+static double CalculateError  (double*,double*,double*,double*,double*,int,int,int);
+#endif
 
-
-int main()
+int main(int argc, char *argv[])
 {
-  int ierr;
-  ierr = main_serial();
+  int ierr,N;
+#ifdef serial
+  printf("Enter N: ");
+  scanf ("%d",&N);
+  ierr = main_serial(N);
+  if (ierr) fprintf(stderr,"main_mpi() returned with an error code of %d.\n",ierr);
+#else
+  int rank,nproc;
+  MPI_Init(&argc,&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Comm_size(MPI_COMM_WORLD,&nproc);
+  if (!rank) {
+    printf("Enter N: ");
+    scanf ("%d",&N);
+  }
+  MPI_Bcast(&N,1,MPI_INT,0,MPI_COMM_WORLD);
+  ierr = main_mpi(N,rank,nproc);
+  if (ierr) fprintf(stderr,"main_mpi() returned with an error code of %d on rank %d.\n",ierr,rank);
+  MPI_Finalize();
+#endif
+  return(0);
 }
 
-
-int main_serial()
+#ifdef serial
+int main_serial(int N)
 {
   double *a1,*b1,*c1,*x;
   double *a2,*b2,*c2,*y;
-  int     N,i,ierr=0;
+  int     i,ierr=0;
   double  error,walltime;
   clock_t start,end;
 
   srand(time(NULL));
 
-  printf("Enter N: ");
-  scanf ("%d",&N);
   printf("Testing serial tridiagLU() with N=%d\n",N);
 
   /* allocate arrays */
@@ -118,6 +142,119 @@ int main_serial()
   free(y);
   return(0);
 }
+#else
+int main_mpi(int N,int rank,int nproc)
+{
+  double *a1,*b1,*c1,*x;
+  double *a2,*b2,*c2,*y;
+  int     i,ierr=0,nlocal;
+  double  error,total_error,walltime;
+  clock_t start,end;
+
+  srand(time(NULL));
+
+  if (!rank) printf("Testing MPI tridiagLU() with N=%d on %d processes\n",N,nproc);
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  /* find local size */
+  ierr = partition1D(N,nproc,rank,&nlocal);
+  printf("Rank: %10d,\tLocal Size: %10d\n",rank,nlocal);
+
+  /* allocate arrays */
+  a1 = (double*) calloc (nlocal,sizeof(double));
+  b1 = (double*) calloc (nlocal,sizeof(double));
+  c1 = (double*) calloc (nlocal,sizeof(double));
+  a2 = (double*) calloc (nlocal,sizeof(double));
+  b2 = (double*) calloc (nlocal,sizeof(double));
+  c2 = (double*) calloc (nlocal,sizeof(double));
+  x  = (double*) calloc (nlocal,sizeof(double));
+  y  = (double*) calloc (nlocal,sizeof(double));
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  /* Test 1: [I]x = b => x = b */
+  for (i = 0; i < nlocal; i++) {
+    a1[i] = 0.0;
+    b1[i] = 1.0;
+    c1[i] = 0.0;
+    x[i]  = rand();
+  }
+  CopyArray(a1,a2,nlocal);
+  CopyArray(b1,b2,nlocal);
+  CopyArray(c1,c2,nlocal);
+  CopyArray(x ,y ,nlocal);
+  /* solve */  
+  if (!rank)  printf("MPI test 1 ([I]x = b => x = b):        \t");
+  start = clock();  ierr = tridiagLU(a1,b1,c1,x,nlocal,rank,nproc);  end = clock();
+  walltime = ((double) (end-start)) / ((double) CLOCKS_PER_SEC);
+  if (ierr == -1) printf("Error - system is singular on process %d\t",rank);
+  /* calculate error */
+  error = CalculateError(a2,b2,c2,y,x,nlocal,rank,nproc);
+  if (nproc > 1)  MPI_Allreduce(&error,&total_error,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+  else            total_error = error;
+  if (!rank)  printf("error=%E\twalltime=%E\n",total_error,walltime);
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  /* Test 2: [U]x = b => x = [U]^(-1)b */
+  for (i = 0; i < nlocal; i++) {
+    a1[i] = 0.0;
+    b1[i] = 0.5;
+    if (rank == nproc-1) c1[i] = (i == nlocal-1 ? 0 : 0.5);
+    else                 c1[i] = 0.5;
+    x[i]  = 1.0;
+  }
+  CopyArray(a1,a2,nlocal);
+  CopyArray(b1,b2,nlocal);
+  CopyArray(c1,c2,nlocal);
+  CopyArray(x ,y ,nlocal);
+  /* solve */  
+  if (!rank) printf("MPI test 2 ([U]x = b => x = [U]^(-1)b):\t");
+  start = clock();  ierr  = tridiagLU(a1,b1,c1,x,nlocal,rank,nproc);  end = clock();
+  walltime = ((double) (end-start)) / ((double) CLOCKS_PER_SEC);
+  if (ierr == -1) printf("Error - system is singular on process %d\t",rank);
+  /* calculate error */
+  error = CalculateError(a2,b2,c2,y,x,nlocal,rank,nproc);
+  if (nproc > 1)  MPI_Allreduce(&error,&total_error,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+  else            total_error = error;
+  if (!rank) printf("error=%E\twalltime=%E\n",total_error,walltime);
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  /* Test 3: [A]x = b => x = [A]^(-1)b */
+  for (i = 0; i < nlocal; i++) {
+    if (!rank )           a1[i] = (i == 0 ? 0.0 : ((double) rand()) / ((double) RAND_MAX));
+    else                  a1[i] = ((double) rand()) / ((double) RAND_MAX);
+    b1[i] = 1.0 + ((double) rand()) / ((double) RAND_MAX);
+    if (rank == nproc-1)  c1[i] = (i == nlocal-1 ? 0 : ((double) rand()) / ((double) RAND_MAX));
+    else                  c1[i] = ((double) rand()) / ((double) RAND_MAX);
+    x[i]  = ((double) rand()) / ((double) RAND_MAX);
+  }
+  CopyArray(a1,a2,nlocal);
+  CopyArray(b1,b2,nlocal);
+  CopyArray(c1,c2,nlocal);
+  CopyArray(x ,y ,nlocal);
+  /* solve */  
+  if (!rank) printf("MPI test 3 ([A]x = b => x = [A]^(-1)b):\t");
+  start = clock();  ierr = tridiagLU(a1,b1,c1,x,nlocal,rank,nproc);  end   = clock();
+  walltime = ((double) (end-start)) / ((double) CLOCKS_PER_SEC);
+  if (ierr == -1) printf("Error - system is singular on process %d\t",rank);
+  /* calculate error */
+  error = CalculateError(a2,b2,c2,y,x,nlocal,rank,nproc);
+  if (nproc > 1)  MPI_Allreduce(&error,&total_error,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+  else            total_error = error;
+  if (!rank) printf("error=%E\twalltime=%E\n",total_error,walltime);
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  /* deallocate arrays */
+  free(a1);
+  free(b1);
+  free(c1);
+  free(a2);
+  free(b2);
+  free(c2);
+  free(x);
+  free(y);
+  return(0);
+}
+#endif
 
 void CopyArray(double *x,double *y,int N)
 {
@@ -126,10 +263,12 @@ void CopyArray(double *x,double *y,int N)
   return;
 }
 
+#ifdef serial
 double CalculateError(double *a,double *b,double *c,double *y,double *x,int N)
 {
-  int i;
   double error = 0;
+  int i;
+  error = 0;
   for (i = 0; i < N; i++) {
     double val;
     if (i == 0)         val = y[i] - (b[i]*x[i]+c[i]*x[i+1]);
@@ -139,4 +278,61 @@ double CalculateError(double *a,double *b,double *c,double *y,double *x,int N)
   }
   return(error);
 }
+#else
+double CalculateError(double *a,double *b,double *c,double *y,double *x,int N,int rank,int nproc)
+{
+  double        error = 0;
+  int           i,nreq;
+  double        xp1, xm1; /* solution from neighboring processes */
+  MPI_Status    *status;
+  MPI_Request   *request;
 
+  xp1 = 0;
+  if (nproc > 1) {
+    nreq = ((rank == 0 || rank == nproc-1) ? 1 : 2);
+    request  = (MPI_Request*) calloc (nreq,sizeof(MPI_Request));
+    status   = (MPI_Status*)  calloc (nreq,sizeof(MPI_Status));
+    if (rank)                   MPI_Isend(&x[0],1,MPI_DOUBLE,rank-1,1738,MPI_COMM_WORLD,&request[0]);
+    if (!rank)                  MPI_Irecv(&xp1 ,1,MPI_DOUBLE,rank+1,1738,MPI_COMM_WORLD,&request[0]);
+    else if (rank != nproc-1)   MPI_Irecv(&xp1 ,1,MPI_DOUBLE,rank+1,1738,MPI_COMM_WORLD,&request[1]);
+    MPI_Waitall(nreq,&request[0],&status[0]);
+    free(request);
+    free(status);
+  }
+  
+  xm1 = 0;
+  if (nproc > 1) {
+    nreq = ((rank == 0 || rank == nproc-1) ? 1 : 2);
+    request  = (MPI_Request*) calloc (nreq,sizeof(MPI_Request));
+    status   = (MPI_Status*)  calloc (nreq,sizeof(MPI_Status));
+    if (rank != nproc-1)  MPI_Isend(&x[N-1],1,MPI_DOUBLE,rank+1,1739,MPI_COMM_WORLD,&request[0]);
+    if (rank == nproc-1 ) MPI_Irecv(&xm1   ,1,MPI_DOUBLE,rank-1,1739,MPI_COMM_WORLD,&request[0]);
+    else if (rank)        MPI_Irecv(&xm1   ,1,MPI_DOUBLE,rank-1,1739,MPI_COMM_WORLD,&request[1]);
+    MPI_Waitall(nreq,&request[0],&status[0]);
+    free(request);
+    free(status);
+  }
+
+  error = 0;
+  for (i = 0; i < N; i++) {
+    double val;
+    if (i == 0)         val = y[i] - (a[i]*xm1   +b[i]*x[i]+c[i]*x[i+1]);
+    else if (i == N-1)  val = y[i] - (a[i]*x[i-1]+b[i]*x[i]+c[i]*xp1   );
+    else                val = y[i] - (a[i]*x[i-1]+b[i]*x[i]+c[i]*x[i+1]);
+    error += val * val;
+  }
+  return(error);
+}
+#endif
+
+#ifndef serial
+int partition1D(int nglobal,int nproc,int rank,int* nlocal)
+{
+  if (nglobal%nproc == 0) *nlocal = nglobal/nproc;
+  else {
+    if (rank == nproc-1)  *nlocal = nglobal/nproc+nglobal%nproc;
+    else                  *nlocal = nglobal/nproc;
+  }
+  return(0);
+}
+#endif
--- src/TridiagLU/tridiagLU.c
+++ src/TridiagLU/tridiagLU.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #ifndef serial
 #include <mpi.h>
 #endif
@@ -6,13 +6,13 @@
 /* Parallel direct solver for tridiagonal systems */
 /*
   Arguments:-
-    a   [0,n-1] double  subdiagonal entries
-    b   [0,n-1] double  diagonal entries
-    c   [0,n-1] double  superdiagonal entries
-    x   [0,n-1] double  right-hand side
-    n           int     local size of the system
-    rank        int     rank of this process
-    nproc       int     total number of processes
+    a   [0,n-1] double*   subdiagonal entries
+    b   [0,n-1] double*   diagonal entries
+    c   [0,n-1] double*   superdiagonal entries
+    x   [0,n-1] double*   right-hand side
+    n           int       local size of the system
+    rank        int       rank of this process
+    nproc       int       total number of processes
 
   Output:-
     x will contain the solution at the end of this function
@@ -37,8 +37,8 @@ int tridiagLU(double *a,double *b,double *c,double *x,int n,int rank,int nproc)
   int         istart,iend;
   double      sendbuf[4],recvbuf[4];
 #ifndef serial
-  MPI_Request request[2];
-  MPI_Status  status [2];
+  MPI_Request *request;
+  MPI_Status  *status;
 #endif
 
   /* Stage 1 - Parallel elimination of subdiagonal entries */
@@ -53,24 +53,31 @@ int tridiagLU(double *a,double *b,double *c,double *x,int n,int rank,int nproc)
     if (rank) {
       double factor = c[0] / b[i-1];
       c[0]  = -factor * c[i-1];
+      b[0] -=  factor * a[i-1];
       x[0] -=  factor * x[i-1];
     }
   }
 
   /* Stage 2 - Eliminate the first sub- & super-diagonal entries */
   /* This needs the last (a,b,c) from the previous process       */
-  sendbuf[0] = a[n-1]; sendbuf[1] = b[n-1]; sendbuf[2] = c[n-1]; sendbuf[4] = x[n-1];
+  sendbuf[0] = a[n-1]; sendbuf[1] = b[n-1]; sendbuf[2] = c[n-1]; sendbuf[3] = x[n-1];
 #ifndef serial
   if (nproc > 1) {
+    int nreq = ((rank == 0 || rank == nproc-1) ? 1 : 2);
+    request  = (MPI_Request*) calloc (nreq,sizeof(MPI_Request));
+    status   = (MPI_Status*)  calloc (nreq,sizeof(MPI_Status));
     if (rank != nproc-1)  MPI_Isend(&sendbuf[0],4,MPI_DOUBLE,rank+1,1436,MPI_COMM_WORLD,&request[0]);
-    if (rank)             MPI_Irecv(&recvbuf[0],4,MPI_DOUBLE,rank-1,1436,MPI_COMM_WORLD,&request[1]);
-    MPI_Waitall(2,request,status);
+    if (rank == nproc-1 ) MPI_Irecv(&recvbuf[0],4,MPI_DOUBLE,rank-1,1436,MPI_COMM_WORLD,&request[0]);
+    else if (rank)        MPI_Irecv(&recvbuf[0],4,MPI_DOUBLE,rank-1,1436,MPI_COMM_WORLD,&request[1]);
+    MPI_Waitall(nreq,&request[0],&status[0]);
+    free(request);
+    free(status);
   }
 #endif
-  double am1, bm1, cm1, xm1;
-  am1 = recvbuf[0]; bm1 = recvbuf[1]; cm1 = recvbuf[2]; xm1 = recvbuf[3];
   /* The first process sits this one out */
   if (rank) {
+    double am1, bm1, cm1, xm1;
+    am1 = recvbuf[0]; bm1 = recvbuf[1]; cm1 = recvbuf[2]; xm1 = recvbuf[3];
     double factor;
     if (bm1 == 0) return(-1);
     factor =  a[0] / bm1;
@@ -85,19 +92,53 @@ int tridiagLU(double *a,double *b,double *c,double *x,int n,int rank,int nproc)
   }
 
 
-  /* Stage 3 - Solve the reduced (nproc-1) X (nproc-1) tridiagonal system */
+  /* Stage 3 - Solve the reduced (nproc-1) X (nproc-1) tridiagonal system   */
+  /**** BAD IMPLEMENTATION - solving this on ALL processes except the first */
+  double xp1 = 0.0; /* solution for the first element on the next process   */
+  if (nproc > 1) {
+    double *ra,*rb,*rc,*rx; /* arrays for the reduced tridiagonal system      */
 
+    /* allocate the arrays */
+    ra = (double*) calloc (nproc-1, sizeof(double));
+    rb = (double*) calloc (nproc-1, sizeof(double));
+    rc = (double*) calloc (nproc-1, sizeof(double));
+    rx = (double*) calloc (nproc-1, sizeof(double));
+
+    /* set this process' element of these arrays and rest to zero */
+    for (i = 0; i < nproc-1; i++) ra[i] = rb[i] = rc[i] = rx[i] = 0.0;
+    if (rank) {
+      ra[rank-1] = a[0]; 
+      rb[rank-1] = b[0];
+      rc[rank-1] = c[0];
+      rx[rank-1] = x[0];
+    }
+
+    /* assemble the complete arrays across all processes */
+#ifndef serial
+    if (nproc > 1) MPI_Allreduce(ra,ra,nproc-1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+    if (nproc > 1) MPI_Allreduce(rb,rb,nproc-1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+    if (nproc > 1) MPI_Allreduce(rc,rc,nproc-1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+    if (nproc > 1) MPI_Allreduce(rx,rx,nproc-1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+#endif
+
+    /* solve the system independently on all the process */
+    if (nproc-1 > 1) {
+      ierr = tridiagLU(ra,rb,rc,rx,nproc-1,0,1);
+      if (ierr) return(ierr);
+    } else rx[0] /= rb[0];
+
+    /* save the solution */
+    if (rank) x[0] = rx[rank-1];
+    if (rank != nproc-1)  xp1 = rx[rank];
+
+    /* clean up */
+    free(ra);
+    free(rb);
+    free(rc);
+    free(rx);
+  }
 
   /* Stage 4 - Parallel back-substitution to get the solution  */
-  double xp1 = 0.0;
-  sendbuf[0] = x[0];
-#ifndef serial
-  if (nproc > 1) {
-    if (rank)             MPI_Isend(&sendbuf[0],1,MPI_DOUBLE,rank-1,1538,MPI_COMM_WORLD,&request[0]);
-    if (rank != nproc-1)  MPI_Irecv(&xp1       ,1,MPI_DOUBLE,rank+1,1538,MPI_COMM_WORLD,&request[1]);
-    MPI_Waitall(2,&request[0],status);
-  }
-#endif
   istart = n-1;
   iend   = (rank == 0 ? 0 : 1);
   if (b[istart] == 0) return(-1);
@@ -107,5 +148,26 @@ int tridiagLU(double *a,double *b,double *c,double *x,int n,int rank,int nproc)
     x[i] = (x[i]-c[i]*x[i+1]-a[i]*x[0]) / b[i];
   }
 
+  /* Done - now x contains the solution */
   return(0);
 }
+
+
+
+
+  /*
+  sendbuf[0] = x[0];
+#ifndef serial
+  if (nproc > 1) {
+    int nreq = ((rank == 0 || rank == nproc-1) ? 1 : 2);
+    request  = (MPI_Request*) calloc (nreq,sizeof(MPI_Request));
+    status   = (MPI_Status*)  calloc (nreq,sizeof(MPI_Status));
+    if (rank)                  MPI_Isend(&sendbuf[0],1,MPI_DOUBLE,rank-1,1538,MPI_COMM_WORLD,&request[0]);
+    if (!rank)                 MPI_Irecv(&xp1       ,1,MPI_DOUBLE,rank+1,1538,MPI_COMM_WORLD,&request[0]);
+    else if (rank != nproc-1)  MPI_Irecv(&xp1       ,1,MPI_DOUBLE,rank+1,1538,MPI_COMM_WORLD,&request[1]);
+    MPI_Waitall(nreq,&request[0],&status[0]);
+    free(request);
+    free(status);
+  }
+#endif
+  */
